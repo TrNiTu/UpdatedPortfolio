5f699676dd3d45e07d38644f071abd62184992fb
Trey T <tturner@huskers.unl.edu>
2021-02-04 15:33:01 -0600

    suppressed seemingly useless JS error - close #11

--- src/js/initialHeaderHandler.js
+++ src/js/initialHeaderHandler.js
@@ -1,18 +1,24 @@
 const header = "Hello, I'm Trey.";
 let count = 0;
 let index = 0;
-let currentText = '';
-let letter = '';
+let letter = "";
+
+/* Silences errors*/
+function silentErrorHandler() {
+    return true;
+}
 
 (function initialHeaderHandler() {
+    var currentText = "";
 
-    currentText = header[count];
-    letter = currentText.slice(0, ++index);
+        currentText = header[count];
+        letter = currentText.slice(0, ++index);
 
-    document.querySelector('.initial-header').textContent += letter;
-    if(letter.length === currentText.length) {
-        count++;
-    }
+        document.querySelector(".initial-header").textContent += letter;
+        if(letter.length === currentText.length) {
+            count++;
+        }
 
+    window.onerror = silentErrorHandler;
     setTimeout(initialHeaderHandler, 75);
 }());
